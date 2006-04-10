9277e48d62c2f61b207fd41a8bd6d2ab55ee4e35
loganallred <loganallred@2a4b192d-170f-0410-9940-9fa4d9b264c7>
2006-04-10 06:57:06 +0000

    update copyright fix compiler warning
    git-svn-id: svn://svn.code.sf.net/p/macpaf/code/trunk/MacPAF@100 2a4b192d-170f-0410-9940-9fa4d9b264c7

--- main.m
+++ main.m
@@ -3,12 +3,12 @@
 //  MacPAF
 //
 //  Created by Logan Allred on Tue Feb 10 2004.
-//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2003-2006 RedBugz Software. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
 {
-    return NSApplicationMain(argc, argv);
+    return NSApplicationMain(argc, (const char **) argv);
 }

125418e429a3b8bc41f15018bde0db26d4d21276
C.W. Betts <computers57@hotmail.com>
2013-10-13 17:56:14 -0600

    Port the Java class MainToolbar to Objective-C as MainToolbarDelegate.

---
+++ Objective C Classes/MainToolbarDelegate.h
@@ -0,0 +1,13 @@
+//
+//  MainToolbarDelegate.h
+//  MAF
+//
+//  Created by C.W. Betts on 10/13/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+//TODO: Embed in XIB. The add objects will need the most work.
+@interface MainToolbarDelegate : NSToolbar <NSToolbarDelegate>
+
+@end

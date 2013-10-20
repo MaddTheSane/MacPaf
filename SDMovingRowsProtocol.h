96caee45e9aef090a38263c6116f3243d8e7cefd
MaddTheSane <computers57@hotmail.com>
2013-10-20 15:31:11 -0600

    Update SDMovingRowsProtocol.h

--- SDMovingRowsProtocol.h
+++ SDMovingRowsProtocol.h
@@ -15,29 +15,30 @@
 // pass this pack for
 
 #define DRAG_ALWAYS    0
-- (unsigned int)dragReorderingMask:(int)forColumn;
+- (NSUInteger)dragReorderingMask:(NSInteger)forColumn;
 
 // Delegate called after the reordering of cells, you must reorder your data.
 // Returning YES will cause the table to be reloaded.
-- (BOOL)tableView:(NSTableView *)tv didDepositRow:(int)rowToMove at:(int)newPosition;
+- (BOOL)tableView:(NSTableView *)tv didDepositRow:(NSInteger)rowToMove at:(NSInteger)newPosition;
 
 // This gives you a chance to decline to drop particular rows on other particular
 // row. Return YES if you don't care
-- (BOOL) tableView:(SDTableView *)tableView draggingRow:(int)draggedRow overRow:(int) targetRow;
+- (BOOL) tableView:(SDTableView *)tableView draggingRow:(NSInteger)draggedRow overRow:(NSInteger) targetRow;
 
 
+@optional
 
 // This is optional: the delegate can decide whether it wants to allow a
 // particular row to be moved
 
-//- (BOOL) tableView:(SDTableView *)tableView rowWillMove:(int)draggedRow;
+- (BOOL) tableView:(SDTableView *)tableView rowWillMove:(NSInteger)draggedRow;
 
 // This is optional: the delegate can return an image representing a row
 
-//- (NSImage *)tableView:(SDTableView *)tableView imageForRow:(int)aRow;
+- (NSImage *)tableView:(SDTableView *)tableView imageForRow:(NSInteger)aRow;
 
 // This is optional: the delegate can refuse to drop a row over another row
 
-// - (BOOL) tableView:(SDTableView *)tableView willDepositRow:(int) rowToMove at:(int) newPosition;
+ - (BOOL) tableView:(SDTableView *)tableView willDepositRow:(NSInteger) rowToMove at:(NSInteger) newPosition;
 
 @end
